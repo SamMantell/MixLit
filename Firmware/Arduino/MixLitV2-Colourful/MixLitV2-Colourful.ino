ddbe50afbb09183eb227385b65a24f111f0e1469
AlpacasRule <mantellsam@gmail.com>
2024-01-08 21:56:11 +0000

    Made Cool Blue Colours
    Also some optimisations here and there

--- Firmware/Arduino/MixLitV2-Colourful/MixLitV2-Colourful.ino
+++ Firmware/Arduino/MixLitV2-Colourful/MixLitV2-Colourful.ino
@@ -16,7 +16,7 @@ GitHub: @SamMantell, @Goddeh1
 #define NUM_OF_LED_STRIPS 5
 #define NUM_OF_LEDS_PER_STRIP 8
 
-uint8_t colorIndex = 0;
+uint16_t colorIndex = 0;
 
 const int NumOfSliders = 5;
 char SliderIDs[NumOfSliders] = {'A', 'A', 'A', 'A', 'A'};
@@ -32,25 +32,25 @@ CRGB leds[NUM_OF_LED_STRIPS][NUM_OF_LEDS_PER_STRIP];
 
 void setLEDs(int iCurrentValue, uint8_t red, uint8_t green, uint8_t blue, int ledStrip)
 {
-  uint8_t NewColorIndex = colorIndex + ledStrip*51;
-  int iNumOfLedsOn = iCurrentValue / 128;
-  int iFinalLedBrightness = (iCurrentValue % 128) * 2;
+  uint16_t NewColorIndex = colorIndex + ledStrip*51;
+  uint8_t iNumOfLedsOn = iCurrentValue >> 7;
+  uint8_t iFinalLedBrightness = (iCurrentValue % 128) << 1;
 
   for (int i = 0; i < 8; i++)
   {
     if (i == (7 - iNumOfLedsOn))
     {
-      leds[ledStrip][i] = ColorFromPalette( RainbowColors_p, NewColorIndex, iFinalLedBrightness, LINEARBLEND);
+      leds[ledStrip][i] = ColorFromPalette( OceanColors_p , NewColorIndex, iFinalLedBrightness, LINEARBLEND);
       //leds[ledStrip][i] = CRGB(iFinalLedBrightness, iFinalLedBrightness, iFinalLedBrightness);
     }
     else if (i > (7 - iNumOfLedsOn))
     {
-      leds[ledStrip][i] = ColorFromPalette( RainbowColors_p, NewColorIndex, 255, LINEARBLEND);
+      leds[ledStrip][i] = ColorFromPalette( OceanColors_p , NewColorIndex, 255, LINEARBLEND);
       //leds[ledStrip][i] = CRGB(red, green, blue);
     }
     else
     {
-      leds[ledStrip][i] = ColorFromPalette( RainbowColors_p, NewColorIndex, 0, LINEARBLEND);
+      leds[ledStrip][i] = ColorFromPalette( OceanColors_p , NewColorIndex, 0, LINEARBLEND);
       //leds[ledStrip][i] = CRGB(0, 0, 0);
     }
   }
@@ -66,12 +66,12 @@ void setup()
   }
 
   FastLED.addLeds<NEOPIXEL, 7>(leds[0], NUM_OF_LEDS_PER_STRIP).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<NEOPIXEL, 6>(leds[1], NUM_OF_LEDS_PER_STRIP).setCorrection( TypicalLEDStrip );
-  FastLED.addLeds<NEOPIXEL, 5>(leds[2], NUM_OF_LEDS_PER_STRIP).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<NEOPIXEL, 5>(leds[1], NUM_OF_LEDS_PER_STRIP).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<NEOPIXEL, 6>(leds[2], NUM_OF_LEDS_PER_STRIP).setCorrection( TypicalLEDStrip );
   FastLED.addLeds<NEOPIXEL, 4>(leds[3], NUM_OF_LEDS_PER_STRIP).setCorrection( TypicalLEDStrip );
   FastLED.addLeds<NEOPIXEL, 3>(leds[4], NUM_OF_LEDS_PER_STRIP).setCorrection( TypicalLEDStrip );
 
-  FastLED.setBrightness(40);
+  FastLED.setBrightness(64);
 }
 
 void loop()
@@ -108,8 +108,5 @@ void loop()
   }
 
   delay(20);
-  colorIndex++;
-  colorIndex++;
-  colorIndex++;
   colorIndex++;
 }
