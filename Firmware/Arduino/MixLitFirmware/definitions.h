51c0672e3929bf210de7184f6a39e3ffa3bf5ba2
Sam M <96017692+SamMantell@users.noreply.github.com>
2025-05-25 14:25:17 +0100

    beginning to work with classes

---
+++ Firmware/Arduino/MixLitFirmware/definitions.h
@@ -0,0 +1,7 @@
+// definitions for the MixLit
+
+#define NUM_OF_SLIDERS 5
+#define NUM_OF_POTENTIOMETERS 3
+#define NUM_OF_LED_STRIPS 5
+#define NUM_OF_LEDS_PER_STRIP 8
+#define NUM_OF_BUTTONS 5
---
+++ Firmware/Arduino/MixLitFirmware/mixlit.h
@@ -0,0 +1,75 @@
+#include <FastLED.h>
+#include "definitions.h"
+
+class mixlit {
+  public:
+    const int sliders[NUM_OF_SLIDERS] = {A4, A3, A2, A1, A0};
+    const int potentiometers[NUM_OF_POTENTIOMETERS] = {A5, A7, A6};
+    const int ledStrips[NUM_OF_LED_STRIPS] = {11, 10, 9, 8, 7};
+    const int buttons[NUM_OF_BUTTONS] = {6, 5, 4, 3, 2};
+    const String buttonNames[NUM_OF_BUTTONS] = {"A", "B", "C", "D", "E"};
+
+    int previousSliderState[NUM_OF_SLIDERS];
+    int currentSliderState[NUM_OF_SLIDERS];
+
+    int currentPotentiometerState[NUM_OF_POTENTIOMETERS];
+    int previousPotentiometerState[NUM_OF_POTENTIOMETERS];
+
+    bool previousButtonState[NUM_OF_BUTTONS];
+    bool currentButtonState[NUM_OF_BUTTONS];
+
+    CRGB leds[NUM_OF_LED_STRIPS][NUM_OF_LEDS_PER_STRIP];
+
+    bool isAnimated = false;
+    uint8_t ColorIndex;
+    int colorIndexOffset;
+
+    CRGBPalette16 All_ColorPallete[NUM_OF_LED_STRIPS] = 
+    {
+      CRGBPalette16   (
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF,
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF
+                      ),
+      CRGBPalette16   (
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0xFF0000, 0xFF0000,  0xFF0000,
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF
+                      ),
+      CRGBPalette16   (
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0x00FF00, 0x00FF00,  0x00FF00,
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF
+                      ),
+      CRGBPalette16   (
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0x0000FF, 0x0000FF,  0x0000FF,
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF
+                      ),
+      CRGBPalette16   (
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0xFF00FF, 0xFF00FF,  0xFF00FF,
+                        0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF,  0xFFFFFF
+                      )
+    };
+
+    int sliderToChange;
+    bool needsUpdate;
+
+    void setLEDs(int iCurrentValue, int ledStrip)
+    {
+      // if (isAnimated) colorIndexOffset++;
+
+      // else colorIndexOffset = 0;
+
+      // this will take the 10 bit value from the slider, and use bitshift and remainder calculation to get the number of leds on and the brightness of the final one.
+      uint8_t iNumOfLedsOn = iCurrentValue >> 7;
+      uint8_t iFinalLedBrightness = (iCurrentValue % 128) << 1;
+
+      for (int i = 0; i < 8; i++)
+      {
+        ColorIndex = 16*i/* + colorIndexOffset*/;
+
+        if (i == (NUM_OF_LEDS_PER_STRIP - 1 - iNumOfLedsOn))        leds[ledStrip][i] = ColorFromPalette( All_ColorPallete[ledStrip], ColorIndex, iFinalLedBrightness);
+      
+        else if (i > (NUM_OF_LEDS_PER_STRIP - 1 - iNumOfLedsOn))    leds[ledStrip][i] = ColorFromPalette( All_ColorPallete[ledStrip], ColorIndex, 255);
+          
+        else                                                        leds[ledStrip][i] = ColorFromPalette( All_ColorPallete[ledStrip], ColorIndex, 0);
+      }
+    }
+};
